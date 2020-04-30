932aa3bb7f56c4f566babc0db34b7470ae353f13
michele <mhallakstamler@gmail.com>
2020-04-30 18:11:03 +0300

    Support Arm Hight & Blade Pitch

--- Assets/ArmHightPublisher.cs
+++ Assets/ArmHightPublisher.cs
@@ -8,20 +8,19 @@ namespace RosSharp.RosBridgeClient
         //public float Rate;
 
         private Messages.Standard.Int32 message;
-       // private Transform Parent;
+        // private Transform Parent;
         public LayerMask LayerMask;
 
         protected override void Start()
         {
             base.Start();
-          //  Parent = FindObjectOfType<TankDriver>().transform;
+            //  Parent = FindObjectOfType<TankDriver>().transform;
             InitializeMessage();
         }
 
         void FixedUpdate()
         {
-
-            UpdateMessage();
+                        UpdateMessage();
         }
 
 
@@ -34,18 +33,43 @@ namespace RosSharp.RosBridgeClient
 
         private void UpdateMessage()
         {
+
+         //   down 43-->  145
+         //   UP   59 --> 275
+
+            // ReayCast(); //Dont Need raycast any more
+
+            //we gonna use the Y Rotation to measure the hight
+            float hight=transform.localEulerAngles.y;
+           // Debug.Log("HIGHT :" + hight);
+            message.data = CalcRange(hight);
+            Publish(message);
+        }
+
+        private int CalcRange(float val)
+        {
+
+            val = 8.1258f * val - 204.375f;
+            Debug.Log("Hight:" + (int)val);
+            return (int)val;
+            
+
+        }
+
+
+        private void ReayCast()
+        {
             RaycastHit hit;
-          
+
             if (Physics.Raycast(transform.position, Vector3.down, out hit, Mathf.Infinity, LayerMask))
             {
                 Debug.DrawLine(transform.position, hit.point, Color.blue);
-// Debug.Log("distance" + hit.distance);
+                // Debug.Log("distance" + hit.distance);
                 var flDistance = hit.distance * 100;
                 ValueToPublish = (int)flDistance;
                 message.data = ValueToPublish;
                 Publish(message);
             }
-
         }
     }
 }
